40beb8d81cb9ac6d2c47c4c8536f5e60e262664b
Techlegends1104 <satyamsouravkr2712@gmail.com>
2025-04-12 03:01:04 +0530

    Add files via upload

--- lin.php
+++ lin.php
@@ -20,6 +20,14 @@ $resp=curl_exec($curl);
 curl_close($curl);
 $data=json_decode($resp);
 
+if ($otp == $_SESSION['OTP']) {
+    echo "OTP Verified Successfully!";
+    // You can now redirect to dashboard or set session login
+    // unset($_SESSION['OTP']); // optional: clear OTP
+} else {
+    echo "Invalid OTP. Please try again.";
+}
 
 
-?> 
+
+?>
